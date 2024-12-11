9338c5cdc80fdae58dc9fb80873b5c5b44c43259
François-Pierre PASQUIER <pitch78@gmail.com>
2024-12-11 21:35:34 +0100

    day 11 - step 2

--- day_11/resolve.py
+++ day_11/resolve.py
@@ -1,5 +1,6 @@
+from memoize.wrapper import memoize
+
 from common.Puzzle import Puzzle
-from common.Step import Step
 
 
 class TodaysPuzzle(Puzzle):
@@ -7,14 +8,8 @@ class TodaysPuzzle(Puzzle):
         super().__init__()
 
     def solve_step1(self) -> tuple[int | str | None, bool]:
-        return self.solve_both_steps()
-
-    def solve_step2(self) -> tuple[int | str | None, bool]:
-        return self.solve_both_steps()
-
-    def solve_both_steps(self) -> tuple[int | str | None, bool]:
         stones = [int(stone_number) for stone_number in self.raw_items[0].split()]
-        nb_loops = 25 if self.step == Step.STEP_1 else 75
+        nb_loops = 25
         for i in range(nb_loops):
             new_arrangement: list[int] = []
             for stone in stones:
@@ -30,8 +25,39 @@ class TodaysPuzzle(Puzzle):
                     new_arrangement.append(2024 * stone)
             stones = new_arrangement.copy()
             self.progress(int(100 / nb_loops * (i + 1)))
-
         return len(stones), True
+
+    def solve_step2(self) -> tuple[int | str | None, bool]:
+        stones = [int(stone_number) for stone_number in self.raw_items[0].split()]
+        result = 0
+        remaining_loop = 75
+        for stone in stones:
+            result += solve_stone(stone, remaining_loop)
+        return result, True
+
+
+cache = {}
+def solve_stone(stone: int, remaining_loop: int) -> int:
+    if remaining_loop == 0:
+        return 1
+
+    if (stone, remaining_loop) in cache:
+        # print(f"cache ({len(cache)}) hit for {stone} at {remaining_loop}")
+        return cache[(stone, remaining_loop)]
+
+    if stone == 0:
+        value = solve_stone(1, remaining_loop - 1)
+        cache[(stone, remaining_loop)] = value
+        return value
+
+    stone_as_str = str(stone)
+    if (len(stone_as_str) % 2) == 0:
+        value = solve_stone(int(stone_as_str[:len(stone_as_str) // 2]), remaining_loop - 1) + solve_stone(int(stone_as_str[len(stone_as_str) // 2:]), remaining_loop - 1)
+        cache[(stone, remaining_loop)] = value
+        return value
+    value = solve_stone(2024 * stone, remaining_loop - 1)
+    cache[(stone, remaining_loop)] = value
+    return value
 
 
 if __name__ == "__main__":
