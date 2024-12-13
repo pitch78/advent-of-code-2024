a4ac737df9fbba67ffedc678006968d9101abf1a
François-Pierre PASQUIER <pitch78@gmail.com>
2024-12-13 10:01:28 +0100

    day 12 - step 2 sides ok for simple rectangles

--- day_12/resolve.py
+++ day_12/resolve.py
@@ -80,7 +80,50 @@ class TodaysPuzzle(Puzzle):
                     self.inspect_region_of(self.enriched_area[plant.y + d[1]][plant.x + d[0]], True, region_char=plant.region_char)
 
     def solve_step2(self) -> tuple[int | str | None, bool]:
+        # 1) get regions as step_1
+        self.enriched_area = self.get_data_as_array(lambda x, y, item: Plant(x, y, item))
+        self.regions = []
+        self.width, self.height = len(self.enriched_area[0]), len(self.enriched_area)
+        for row in range(self.width):
+            for col in range(self.height):
+                self.inspect_region_of(self.enriched_area[row][col])
+                # self.print_regions()
+
+        result = 0
+        for r_index, region in enumerate(self.regions):
+            nb_sides = self.check_sides_of(region)
+            print(f"Region #{r_index}('{region[0].region_char}'): A:{len(region)}, P:{sum([p.external_sides for p in region])}, S: {nb_sides}")
+            result += len(region) * nb_sides
+
+        # 3) détecter les zones imbriquées et ajouter les bords à la zone qui la contient
         return None, False
+
+    def check_sides_of(self, region: list[Plant]) -> int:
+        # 2) go around (meaning 1 plant outside), until looping.
+        # initially one block upper, going left, meaning the first block is on our right
+        initial_pos_x, initial_pos_y = region[0].x + directions[0][0], region[0].y + directions[0][1]
+        initial_dir_index = 1
+        current_pos_x, current_pos_y, current_dir_index = initial_pos_x, initial_pos_y, 1
+        nb_sides = 0
+        current_region_id = region[0].region_id
+        # loop closed?
+        while not (nb_sides > 0 and current_pos_x == initial_pos_x and current_pos_y == initial_pos_y and current_dir_index == initial_dir_index):
+            # we must keep block of the region on our right
+            # so one step in the current direction
+            current_pos_x += directions[current_dir_index][0]
+            current_pos_y += directions[current_dir_index][1]
+            block_on_the_right_x = current_pos_x + directions[(current_dir_index + 1) % 4][0]
+            block_on_the_right_y = current_pos_y + directions[(current_dir_index + 1) % 4][1]
+            if block_on_the_right_x in range(self.width) and block_on_the_right_y in range(self.height):
+                inner_pos = self.enriched_area[block_on_the_right_y][block_on_the_right_x]
+                if inner_pos.region_id == current_region_id:
+                    continue
+
+            # not in the region anymore, we turn right
+            current_dir_index = (current_dir_index + 1) % 4
+            nb_sides += 1
+
+        return nb_sides
 
 
 if __name__ == "__main__":
