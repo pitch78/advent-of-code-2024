175461bb52a263d77552b9fcefe48f90451c7bd0
François-Pierre PASQUIER <pitch78@gmail.com>
2024-12-15 20:30:20 +0100

    day 15 - step 2 - missing move up & down

--- day_15/resolve.py
+++ day_15/resolve.py
@@ -35,13 +35,16 @@ class TodaysPuzzle(Puzzle):
         print()
         print(f"mvt: {ColorUtils.cyan}{mvt}")
         for line in self.room_plan:
-            print("".join(line).replace("@", ColorUtils.BGwhite + ColorUtils.black + ColorUtils.blink +"@"+ColorUtils.reset).replace("O", ColorUtils.yellow+"O").replace("#", ColorUtils.red+"#").replace(".", ColorUtils.reset+"."))
+            print("".join(line)
+                  .replace("@", ColorUtils.BGwhite + ColorUtils.black + ColorUtils.blink + "@" + ColorUtils.reset)
+                  .replace("O", ColorUtils.yellow + "O")
+                  .replace("[]", ColorUtils.yellow + "[]")
+                  .replace("#", ColorUtils.red + "#")
+                  .replace(".", ColorUtils.reset + ".")
+                  )
         print()
         print(f"{ColorUtils.reset}_" * 50)
         print()
-
-    def solve_step2(self) -> tuple[int | str | None, bool]:
-        return None, False
 
     def find_robot_initial_position(self) -> None:
         for y, line in enumerate(self.room_plan):
@@ -77,8 +80,54 @@ class TodaysPuzzle(Puzzle):
         if item_at_pos_forward == "#":
             return
 
-        # space? we move all the block by one (meaning the first one to the final pos effectively)
+        # space? we move all the block one by one (meaning the first one to the final pos effectively)
         self.room_plan[pos_forward[1]][pos_forward[0]] = "O"
+
+        # then the robot
+        self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+        self.robot_position = initial_check_pos
+        self.room_plan[initial_check_pos[1]][initial_check_pos[0]] = "@"
+
+    def move_step2(self, mvt: str) -> None:
+        direction = movements[mvt]
+        pos_forward = self.robot_position[0] + direction[0], self.robot_position[1] + direction[1]
+        item_at_pos_forward = self.room_plan[pos_forward[1]][pos_forward[0]]
+
+        # wall? nothing to do
+        if item_at_pos_forward == "#":
+            return
+
+        # free space? => one step forward
+        if item_at_pos_forward == ".":
+            self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+            self.robot_position = pos_forward
+            self.room_plan[pos_forward[1]][pos_forward[0]] = "@"
+            return
+
+        initial_check_pos = pos_forward
+        while item_at_pos_forward != "#" and item_at_pos_forward != ".":
+            pos_forward = pos_forward[0] + direction[0], pos_forward[1] + direction[1]
+            item_at_pos_forward = self.room_plan[pos_forward[1]][pos_forward[0]]
+
+        # wall? nothing to do
+        if item_at_pos_forward == "#":
+            return
+
+        # space? we move all the block one by one
+        # horizontal move ?
+        if mvt == ">":
+            for x in range(pos_forward[0] - 1, initial_check_pos[0], -1):
+                self.room_plan[pos_forward[1]][x + 1] = self.room_plan[pos_forward[1]][x]
+        elif mvt == "<":
+            for x in range(pos_forward[0], initial_check_pos[0]):
+                self.room_plan[pos_forward[1]][x] = self.room_plan[pos_forward[1]][x + 1]
+        elif mvt == "^":
+            self.move_up([pos_forward, ])
+        elif mvt == "^":
+            pass
+        else:
+            print("unknown move 🤨")
+
 
         # then the robot
         self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
@@ -89,9 +138,29 @@ class TodaysPuzzle(Puzzle):
         gps = 0
         for y, line in enumerate(self.room_plan):
             for x, char in enumerate(line):
-               if char == "O":
+                if char == "O":
                     gps += x + y * 100
         return gps
+
+    def solve_step2(self) -> tuple[int | str | None, bool]:
+        # room plan
+        self.room_plan = [list(line
+                               .replace("#", "##")
+                               .replace("O", "[]")
+                               .replace(".", "..")
+                               .replace("@", "@.")
+                               ) for line in self.raw_items[0].split("\n")]
+        self.find_robot_initial_position()
+
+        # robot instructions
+        self.robot_instructions = []
+        for line in self.raw_items[1].split("\n"):
+            self.robot_instructions.extend(list(line))
+
+        for mvt in self.robot_instructions:
+            self.move_step2(mvt)
+            self.display_plan(mvt)
+        return self.get_blocks_gps(), True
 
 
 if __name__ == "__main__":
