b52d5b6ae4d9ddf514fa8dca94d12a862cea7770
François-Pierre PASQUIER <pitch78@gmail.com>
2024-12-15 20:30:20 +0100

    day 15 - step 2

--- day_15/resolve.py
+++ day_15/resolve.py
@@ -1,4 +1,8 @@
+import os
+from time import sleep
+
 from common.ColorUtils import ColorUtils
+from common.Mode import Mode
 from common.Puzzle import Puzzle
 
 movements = {
@@ -26,22 +30,31 @@ class TodaysPuzzle(Puzzle):
         for line in self.raw_items[1].split("\n"):
             self.robot_instructions.extend(list(line))
 
-        for mvt in self.robot_instructions:
+        for index_mvt, mvt in enumerate(self.robot_instructions):
             self.move(mvt)
-            # self.display_plan(mvt)
         return self.get_blocks_gps(), True
 
-    def display_plan(self, mvt):
-        print()
-        print(f"mvt: {ColorUtils.cyan}{mvt}")
+    def display_plan(self, mvt, index):
+        os.system('clear')
+        print(f"mvt: {ColorUtils.cyan}{mvt} ({index})")
         for line in self.room_plan:
-            print("".join(line).replace("@", ColorUtils.BGwhite + ColorUtils.black + ColorUtils.blink +"@"+ColorUtils.reset).replace("O", ColorUtils.yellow+"O").replace("#", ColorUtils.red+"#").replace(".", ColorUtils.reset+"."))
+            print("".join(line)
+                  .replace("@", ColorUtils.bright + ColorUtils.blue + mvt + ColorUtils.reset)
+                  .replace("O", ColorUtils.yellow + "O")
+                  .replace("[]", ColorUtils.yellow + "[]")
+                  .replace("#", ColorUtils.red + "#")
+                  .replace(".", ColorUtils.reset + ".")
+                  )
         print()
         print(f"{ColorUtils.reset}_" * 50)
         print()
 
-    def solve_step2(self) -> tuple[int | str | None, bool]:
-        return None, False
+    def save_step(self, c, index, file):
+        lines = f"{c} {index}\n"
+        for line in self.room_plan:
+            lines += "".join(line) + "\n"
+        lines += "\n\n"
+        file.writelines(lines)
 
     def find_robot_initial_position(self) -> None:
         for y, line in enumerate(self.room_plan):
@@ -54,44 +67,156 @@ class TodaysPuzzle(Puzzle):
 
     def move(self, mvt: str) -> None:
         direction = movements[mvt]
-        pos_forward = self.robot_position[0] + direction[0], self.robot_position[1] + direction[1]
-        item_at_pos_forward = self.room_plan[pos_forward[1]][pos_forward[0]]
+        pos_ahead = self.robot_position[0] + direction[0], self.robot_position[1] + direction[1]
+        item_at_pos_ahead = self.room_plan[pos_ahead[1]][pos_ahead[0]]
 
         # wall? nothing to do
-        if item_at_pos_forward == "#":
+        if item_at_pos_ahead == "#":
             return
 
         # free space? => one step forward
-        if item_at_pos_forward == ".":
+        if item_at_pos_ahead == ".":
             self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
-            self.robot_position = pos_forward
-            self.room_plan[pos_forward[1]][pos_forward[0]] = "@"
+            self.robot_position = pos_ahead
+            self.room_plan[pos_ahead[1]][pos_ahead[0]] = "@"
             return
 
-        initial_check_pos = pos_forward
-        while item_at_pos_forward != "#" and item_at_pos_forward != ".":
-            pos_forward = pos_forward[0] + direction[0], pos_forward[1] + direction[1]
-            item_at_pos_forward = self.room_plan[pos_forward[1]][pos_forward[0]]
+        initial_check_pos = pos_ahead
+        while item_at_pos_ahead != "#" and item_at_pos_ahead != ".":
+            pos_ahead = pos_ahead[0] + direction[0], pos_ahead[1] + direction[1]
+            item_at_pos_ahead = self.room_plan[pos_ahead[1]][pos_ahead[0]]
 
         # wall? nothing to do
-        if item_at_pos_forward == "#":
+        if item_at_pos_ahead == "#":
             return
 
-        # space? we move all the block by one (meaning the first one to the final pos effectively)
-        self.room_plan[pos_forward[1]][pos_forward[0]] = "O"
+        # space? we move all the block one by one (meaning the first one to the final pos effectively)
+        self.room_plan[pos_ahead[1]][pos_ahead[0]] = "O"
 
         # then the robot
         self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
         self.robot_position = initial_check_pos
         self.room_plan[initial_check_pos[1]][initial_check_pos[0]] = "@"
 
+    def move_step2(self, mvt: str) -> None:
+        direction = movements[mvt]
+        pos_ahead = self.robot_position[0] + direction[0], self.robot_position[1] + direction[1]
+        item_at_pos_ahead = self.room_plan[pos_ahead[1]][pos_ahead[0]]
+
+        # wall? nothing to do
+        if item_at_pos_ahead == "#":
+            return
+
+        # free space? => one step forward
+        if item_at_pos_ahead == ".":
+            self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+            self.robot_position = pos_ahead
+            self.room_plan[pos_ahead[1]][pos_ahead[0]] = "@"
+            return
+
+        initial_check_pos = pos_ahead
+        if mvt in ["<", ">"]:
+            while item_at_pos_ahead != "#" and item_at_pos_ahead != ".":
+                pos_ahead = pos_ahead[0] + direction[0], pos_ahead[1] + direction[1]
+                item_at_pos_ahead = self.room_plan[pos_ahead[1]][pos_ahead[0]]
+
+            # wall? nothing to do
+            if item_at_pos_ahead == "#":
+                return
+
+            # space? we move all the block one by one
+            # horizontal move ?
+            if mvt == ">":
+                for x in range(pos_ahead[0] - 1, initial_check_pos[0] - 1, -1):
+                    self.room_plan[pos_ahead[1]][x + 1] = self.room_plan[pos_ahead[1]][x]
+            else:
+                for x in range(pos_ahead[0], initial_check_pos[0]):
+                    self.room_plan[pos_ahead[1]][x] = self.room_plan[pos_ahead[1]][x + 1]
+
+            # then the robot
+            self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+            self.robot_position = initial_check_pos
+            self.room_plan[initial_check_pos[1]][initial_check_pos[0]] = "@"
+        elif mvt == "^":
+            if self.move_up([pos_ahead]):
+                # then move the robot
+                self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+                self.robot_position = initial_check_pos
+                self.room_plan[pos_ahead[1]][pos_ahead[0]] = "@"
+                # self.display_plan("^")
+        elif mvt == "v":
+            if self.move_down([pos_ahead]):
+                # then move the robot
+                self.room_plan[self.robot_position[1]][self.robot_position[0]] = "."
+                self.robot_position = initial_check_pos
+                self.room_plan[pos_ahead[1]][pos_ahead[0]] = "@"
+                # self.display_plan("v")
+        else:
+            print("unknown move 🤨")
+            exit()
+
+    def move_up(self, positions_to_test: list[tuple[int, int]]) -> bool:
+        return self.move_vertically(-1, positions_to_test)
+
+    def move_down(self, positions_to_test: list[tuple[int, int]]) -> bool:
+        return self.move_vertically(1, positions_to_test)
+
+    def move_vertically(self, offset: int, positions_to_test: list[tuple[int, int]]) -> bool:
+        fixed_positions = []
+        for index, pos in enumerate(positions_to_test):
+            if self.room_plan[positions_to_test[index][1]][positions_to_test[index][0]] == "[":
+                fixed_positions.append(pos)
+                # missing ] ?
+                if index == len(positions_to_test) - 1 or self.room_plan[positions_to_test[index + 1][1]][positions_to_test[index + 1][0]] != "]":
+                    fixed_positions.append((pos[0] + 1, pos[1]))
+            # missing [ ?
+            elif self.room_plan[positions_to_test[index][1]][positions_to_test[index][0]] == "]":
+                if index == 0 or self.room_plan[positions_to_test[index - 1][1]][positions_to_test[index - 1][0]] != "[":
+                    fixed_positions.append((pos[0] - 1, pos[1]))
+                fixed_positions.append(pos)
+
+        # checking next row
+        chars_next_line = [self.room_plan[pos[1] + offset][pos[0]] for pos in fixed_positions]
+        if "#" in chars_next_line:
+            return False
+
+        # not all clear? => need to check, then if not ok => give up
+        if not all(char == "." for char in chars_next_line) and not self.move_vertically(offset, [(p[0], p[1] + offset) for p in fixed_positions]):
+            return False
+
+        # then move the block
+        for pos in fixed_positions:
+            self.room_plan[pos[1] + offset][pos[0]] = self.room_plan[pos[1]][pos[0]]
+            self.room_plan[pos[1]][pos[0]] = "."
+        return True
+
     def get_blocks_gps(self) -> int:
         gps = 0
         for y, line in enumerate(self.room_plan):
             for x, char in enumerate(line):
-               if char == "O":
+                if char in "O[":
                     gps += x + y * 100
         return gps
+
+    def solve_step2(self) -> tuple[int | str | None, bool]:
+        # room plan
+        self.room_plan = [list(line
+                               .replace("#", "##")
+                               .replace("O", "[]")
+                               .replace(".", "..")
+                               .replace("@", "@.")
+                               ) for line in self.raw_items[0].split("\n")]
+        self.find_robot_initial_position()
+
+        # robot instructions
+        self.robot_instructions = []
+        for line in self.raw_items[1].split("\n"):
+            self.robot_instructions.extend(list(line))
+
+        for index_mvt, mvt in enumerate(self.robot_instructions):
+            self.move_step2(mvt)
+        self.display_plan(mvt, index_mvt)
+        return self.get_blocks_gps(), True
 
 
 if __name__ == "__main__":
